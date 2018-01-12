7e4978bedc94ba0d1f7e3b1672c0fa4145cb86e7
Sting Cheng <sting.cheng@gmail.com>
2018-01-12 20:21:36 +0800

    first commit

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+# Module name should match apk name to be installed.
+LOCAL_MODULE := ASwitch
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
---
+++ README.md
@@ -0,0 +1,3 @@
+## ASwitch
+
+ASwitch(Android Switch) is a CBA system App that can be used to switch focusing Android instance between host and container.
